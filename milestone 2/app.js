d9282583f8a87f20e15cf44c350dc01210e08a67
Difelice-e <difelice.emanuele@gmail.com>
2022-03-31 15:13:03 +0200

    stampa dinamica con vue

--- milestone 2/app.js
+++ milestone 2/app.js
@@ -1,10 +1,19 @@
 const app = new Vue ({ 
     el: '#app',
     data: {
-
+        discs: []
     },
 
     methods: {
-
+        fetchDiscs: function() {
+            axios.get('db.php')
+            .then(res => {
+                this.discs = res.data
+                console.log(res.data)
+            })
+        }
     },
+    created() {
+        this.fetchDiscs()
+    }
  })
--- milestone 2/index.php
+++ milestone 2/index.php
@@ -1,76 +1,7 @@
 <?php 
-    $discs = [
-        [
-          'poster' => 'https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg',
-          'title' => 'New Jersey',
-          'author' => 'Bon Jovi',
-          'genre' => 'Rock',
-          'year' => '1988'
-        ],
-      [
-          'poster' => 'https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg',
-          'title' => 'Live at Wembley 86',
-          'author' => 'Queen',
-          'genre' => 'Pop',
-          'year' => '1992'
-        ],
-      [
-          'poster' => 'https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg',
-          'title' => 'Ten\'s Summoner\'s Tales',
-          'author' => 'Sting',
-          'genre' => 'Pop',
-          'year' => '1993'
-        ],
-      [
-          'poster' => 'https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg',
-          'title' => 'Steve Gadd Band',
-          'author' => 'Steve Gadd Band',
-          'genre' => 'Jazz',
-          'year' => '2018'
-        ],
-      [
-          'poster' => 'https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg',
-          'title' => 'Brave new World',
-          'author' => 'Iron Maiden',
-          'genre' => 'Metal',
-          'year' => '2000'
-        ],
-      [
-          'poster' => 'https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg',
-          'title' => 'One more car, one more raider',
-          'author' => 'Eric Clapton',
-          'genre' => 'Rock',
-          'year' => '2002'
-        ],
-      [
-          'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg',
-          'title' => 'Made in Japan',
-          'author' => 'Deep Purple',
-          'genre' => 'Rock',
-          'year' => '1972'
-        ],
-      [
-          'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
-          'title' => 'And Justice for All',
-          'author' => 'Metallica',
-          'genre' => 'Metal',
-          'year' => '1988'
-        ],
-      [
-          'poster' => 'https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg',
-          'title' => 'Hard Wired',
-          'author' => 'Dave Weckl',
-          'genre' => 'Jazz',
-          'year' => '1994'
-        ],
-      [
-          'poster' => 'https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg',
-          'title' => 'Bad',
-          'author' => 'Michael Jacjson',
-          'genre' => 'Pop',
-          'year' => '1987'
-        ]
-      ];
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -81,6 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 <body>
     <div id="app">
@@ -101,27 +34,23 @@
 
                 <div class="container album-wrapper">
 
-                    <?php
-                    foreach ($discs as $value) {
-                    ?>
-                        <div class="album-card card">
+                        <div v-for="disc in discs" class="album-card card">
                             <figure class="album__cover">
-                                <img src="<?php echo $value['poster'] ?>" alt="" />
+                                <img :src="disc.poster" alt="" />
                             </figure>
 
                             <div class="album__info">
-                                <h4 class="album__title"><?php echo $value['title'] ?></h4>
-                                <p class="album__author"><?php echo $value['author'] ?></p>
-                                <p class="album__year"><?php echo $value['year'] ?></p>
+                                <h4 class="album__title">{{disc.title}}</h4>
+                                <p class="album__author">{{disc.author}}</p>
+                                <p class="album__year">{{disc.year}}</p>
                             </div>
-                        </div>   
-                    <?php
-                    }
-                    ?>
+                        </div> 
                     
                 </div>
             </section>
         </main>
     </div>
+
+    <script src="app.js"></script>
 </body>
 </html>
